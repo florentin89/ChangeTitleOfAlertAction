be44424e04264da69fd2948cfd3fd27713163ee6
Florentin Lupascu <lupascu_florentin@yahoo.com>
2019-06-05 13:58:15 +0100

    Add files via upload

---
+++ ChangeTitleOfAlertAction/ViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ViewController.swift
+//  ChangeTitleOfAlertAction
+//
+//  Created by Florentin Lupascu on 05/06/2019.
+//  Copyright © 2019 Florentin Lupascu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        tableView.tableFooterView = UIView()
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = "Row \(indexPath.row)"
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        showOptions()
+    }
+    
+    func showOptions(){
+        
+        var enable = "Enable"
+        let disable = "Disable"
+        
+        let applyOn = UIAlertAction(title: enable, style: .default, handler: { (action: UIAlertAction!) in
+            
+            enable = disable
+        })
+        
+        let actionSheet = configureActionSheet()
+        actionSheet.addAction(applyOn)
+        
+        self.present(actionSheet, animated: true, completion: nil)
+    }
+    
+    func configureActionSheet() -> UIAlertController {
+        let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        actionSheet.addAction(cancel)
+        
+        if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiom.pad ){
+            actionSheet.popoverPresentationController?.sourceView = self.view
+            actionSheet.popoverPresentationController?.sourceRect = CGRect(x: self.view.bounds.midX, y: self.view.bounds.midY, width: 0, height: 0)
+            actionSheet.popoverPresentationController?.permittedArrowDirections = []
+        }
+        
+        return actionSheet
+    }
+}
